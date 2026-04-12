fc8ae70c5a7f6bc66edddfe4e0bbc64d343524f6
FakeZero <sjlimdaa@naver.com>
2026-04-13 00:58:12 +0900

    일단 읽는 것까지는 성공
    문자열이 안읽어짐. 포인터가 저장되어 있어서 포인터 값을 읽어도  내용은 읽을 수가 없다...어떡하지?

--- STL_Homework/2022182034 임성진.cpp
+++ STL_Homework/2022182034 임성진.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <print>
+#include <array>
 using namespace std;
+
+class Player
+{
+private:
+	string name;			// 이름, 길이[3, 15], ['a', 'z']로만 구성
+	int score;				// 점수, 정규분포
+	size_t id;				// 아이디, 겹치는 아이디 있을 수 있음
+	size_t num;				// free store에 확보한 바이트 수
+	unique_ptr<char[]> p;	// free store에 확보한 메모리
+
+	friend ostream& operator<<(ostream& os, const Player& player)
+	{
+		print("이름: {:15}, 아이디: {}, 점수: {}, 자원수: {}\n저장된 글자:",
+			player.name, player.score, player.id, player.num);
+		return os;
+	}
+};
+
+array<Player, 300'0000> playerArr{};
 
 int main()
 {
-	cout << "헬로 월드" << endl;
+	ifstream in{ "2026 STL 과제 파일 - 2022182034", ios::binary };
+	if (not in)
+	{
+		cout << "파일이 존재하지 않습니다." << endl;
+		return 2022182034;
+	}
+
+	in.read((char*)playerArr.data(), sizeof(Player) * playerArr.size());
+	cout << playerArr[0] << endl;
+	cout << playerArr[1] << endl;
 }
