e066c43708d513edb7558804eee308f6f60359d6
FakeZero(Laptop) <sjlimdaa@naver.com>
2026-04-17 08:00:12 +0900

    3번까지는 해결

--- STL_Homework/2022182034 임성진.cpp
+++ STL_Homework/2022182034 임성진.cpp
@@ -3,6 +3,11 @@
 #include <print>
 #include <array>
 #include <ranges>
+#include <algorithm>
+#include <memory>
+#include <numeric>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Player
@@ -19,6 +24,21 @@ public:
 			in.read(p.get(), num);
 		}
 	}
+
+	int getScore() const
+	{
+		return score;
+	}
+
+	size_t getId() const
+	{
+		return id;
+	}
+
+	string getName() const
+	{
+		return name;
+	}
 private:
 	string name;			// 이름, 길이[3, 15], ['a', 'z']로만 구성
 	int score;				// 점수, 정규분포
@@ -28,10 +48,11 @@ private:
 
 	friend ostream& operator<<(ostream& os, const Player& player)
 	{
-		print(os, "이름: {:15}, 아이디: {}, 점수: {}, 자원수: {}\n저장된 글자:{}\n",
+		print(os, "이름: {:15}, 아이디: {}, 점수: {}, 자원수: {}\n저장된 글자:{}",
 			player.name, player.score, player.id, player.num, player.p.get());
 		return os;
 	}
+
 };
 
 array<Player, 300'0000> playerArr{};
@@ -48,6 +69,51 @@ int main()
 	for(auto& player : playerArr)
 		player.read(in);
 
-	for (const auto& player : playerArr | views::take(10000))
+	//1번 문제
+	cout << "1번 문제: 파일에 저장한 모든 Player 정보를 읽어 컨테이너에 저장하라. 제일 마지막 Player의 정보를 다음과 같은 형식으로 화면에 출력하라." << endl;
+	cout << playerArr.back() << endl;
+
+	cout << "\n=========================================\n" << endl;
+
+	//2번 문제
+	cout << "2번 문제: 점수가 가장 큰 Player를 찾아 화면에 출력하라.(동점 모두 출력) Player의 평균 점수를 계산하여 화면에 출력하라." << endl;
+	auto maxPtr = max_element(playerArr.begin(), playerArr.end(), [](const Player& a, const Player& b) {
+		return a.getScore() < b.getScore();
+		});
+	int maxScore = maxPtr->getScore();
+	cout << "최고 점수: " << maxScore << endl;
+	cout << "최고 점수를 받은 Player들:" << endl;
+	for (const auto& player : playerArr | views::filter([maxScore](const Player& a) {
+		return a.getScore() == maxScore;
+		}))
+	{
 		cout << player << endl;
+	}
+	long long averageScore = accumulate(playerArr.begin(), playerArr.end(), 0LL, [](long long sum, Player& player) {
+		return sum + player.getScore();
+		}) / playerArr.size();
+	cout << "평균 점수: " << averageScore << endl;
+
+	cout << "\n=========================================\n" << endl;
+
+	//3번 문제
+	cout << "3번 문제: id가 서로 같은 객체를 찾아 \"같은아이디.txt\"에 기록하라. id가 같은 객체는 모두 몇 개인지 화면에 출력하라. 파일에는 id가 같은 Player 객체의 이름과 아이디를 한 줄 씩 기록한다." << endl;
+	unordered_map<size_t, vector<size_t>> idMap;
+	for (size_t i = 0; i < playerArr.size(); ++i) {
+		idMap[playerArr[i].getId()].push_back(i);
+	}
+
+	ofstream out{ "같은아이디.txt" };
+	size_t duplicateCount = 0;
+	for (const auto& [id, playerIndices] : idMap) {
+		if (playerIndices.size() > 1) {
+			duplicateCount += playerIndices.size();
+			out << id << ':' << playerIndices.size() << "개" << endl;
+			for (size_t index : playerIndices) {
+				out << "이름: " << playerArr[index].getName() << ", 아이디: " << id << endl;
+			}
+			out << "==============================" << endl;
+		}
+	}
+	cout << "id가 같은 객체는 총 " << duplicateCount << "개" << endl;
 }
